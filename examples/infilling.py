47f1fdc2d967e8ad02b37cb24c7a1a8a9ff94466
Jasper <jaspergilley2021@u.northwestern.edu>
2023-09-21 16:31:02 -0700

    working infilling

--- examples/infilling.py
+++ examples/infilling.py
@@ -8,20 +8,23 @@ class Infilling(Model):
     def __init__(self, words):
         super().__init__()
         firstword = words.pop(0)
-        self.context = LMContext(LLM, firstword)
-        self.s = ""
+        self.context = LMContext(LLM, "<|endoftext|>" + firstword)
+        self.generation = 0
+        self.s = firstword
         self.tokenized_words = [LLM.tokenizer.encode(word) for word in words]
     
     async def step(self):
         n = await self.sample(Geometric(0.5)) + 1
+        # generate n tokens
         for _ in range(n):
             self.s += await self.sample(self.context.next_token())
 
-        # observe the next token
+        # add the next token from the tokenized words
         for token in self.tokenized_words.pop(0):
             self.s += await self.observe(self.context.next_token(), token)
 
         print(str(self.s))
+        self.generation += 1
 
         if len(self.tokenized_words) == 0:
             self.observe(self.context.next_token(), LLM.tokenizer.eos_token_id)
@@ -31,11 +34,9 @@ class Infilling(Model):
         return self.context.next_token()
     
 async def main():
-    words = ["<|endoftext|>", "Well, you see, every", " he", " to", " another", "!"]
+    words = ["Well, you see, every", " he", " to", " another", "!"]
     model = Infilling(words)
     particles = await smc_standard(model, 20)
-    # for p in particles:
-    #     print(str(p.lm.s))
 
 if __name__=="__main__":
     asyncio.run(main())
